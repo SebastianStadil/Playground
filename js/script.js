84b418a3da2e109978842e5578d6e25373e0ef47
Sebastian Stadil <github@stadil.org>
2012-04-16 21:40:32 -0700

    Added my first knockout function!

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@
    All Rights Reserved (as if you would copy this...)
 */
 
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+function AppViewModel() {
+    this.firstName = ko.observable("Bert");
+    this.lastName = ko.observable("Bertington");
+    this.fullName = ko.computed(function() {
+        return this.firstName() + " " + this.lastName();
+    }, this);
+    this.capitalizeLastName = function() {
+        this.lastName() = this.lastName().toUpperCase()
+    };
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());
 
 $("a[rel=popover]").popover();
 $('#navbar').scrollspy();
