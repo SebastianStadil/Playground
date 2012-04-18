22770365e2e59d3fba7c4d6c707750d4e26ed9c1
Sebastian Stadil <github@stadil.org>
2012-04-18 12:16:52 -0700

    Added Firebase and made some cosmetic changes

--- js/script.js
+++ js/script.js
@@ -2,21 +2,27 @@
    All Rights Reserved (as if you would copy this...)
 */
 
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+// Activates Firebase
+var firebase = new Firebase('http://gamma.firebase.com/sebastianstadil');
+firebase.set('This is cool shit!');
+firebase.child('char').set('Ulrik');
+
+// This is the *viewmodel* - JavaScript that defines the data and behavior of the UI
 function AppViewModel() {
-    this.firstName = ko.observable("Bert");
-    this.lastName = ko.observable("Bertington");
+    this.firstName = ko.observable("Herp");
+    this.lastName = ko.observable("Derp");
     this.fullName = ko.computed(function() {
         return this.firstName() + " " + this.lastName();
     }, this);
     this.capitalizeLastName = function() {
-        this.lastName() = this.lastName().toUpperCase()
+        this.lastName(this.lastName().toUpperCase());
     };
 }
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
 
+// Turn things on
 $("a[rel=popover]").popover();
 $('#navbar').scrollspy();
 $('.dropdown-toggle').dropdown();
